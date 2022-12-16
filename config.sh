505bd84d274f5b09584437e54940d05d885c5e3b
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2022-12-16 22:04:54 +0700

    Add b_exec

--- config.sh
+++ config.sh
@@ -6,5 +6,13 @@ module_prop_base_module="${MODDIR}/module.prop"
 list_module_base_directory="${MODDIR}/file/list_names.prop"
 directory_to_repack_dat="${path_boongan}"
 
+#if you want to use some elf or library file 
+#we recommend to use our executed lib name "b_exec"
+#for example:
+#b_exec anjay.so
+#bcz some termux user's using 32bit prefix like me
+#and this auto exec means you can exec python,sh,bash, and lib
+#that why is recommended to use our "b_exec" as exec lib
+
 #Add your code below
 
