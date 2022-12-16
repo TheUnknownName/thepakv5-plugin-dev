bd7b0b7b804d3126bcd34adc61f9847320053f7f
TUNBudi06 <70352065+TUNBudi06@users.noreply.github.com>
2022-12-16 21:51:06 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/Bash
+#this configuration for your module
+
+MODDIR="${kdir_mod}" # this for default directory of your path
+module_prop_base_module="${MODDIR}/module.prop"
+list_module_base_directory="${MODDIR}/file/list_names.prop"
+directory_to_repack_dat="${path_boongan}"
+
+#Add your code below
+
